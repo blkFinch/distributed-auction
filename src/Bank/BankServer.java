db0d19be3471437a4d9c90b3dab090c3242f1b57
galen <blkfinch@gmail.com>
2020-11-19 10:48:25 -0700

    bank server can accept connections

--- src/Bank/BankServer.java
+++ src/Bank/BankServer.java
@@ -11,6 +11,12 @@ public class BankServer {
     private Bank bank = new Bank();
 
     public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println(
+                    "Usage: java BankServer <port number>");
+            System.exit(1);
+        }
+
         //I'm setting this port 6000 in my config -gh
         int portNumber = Integer.parseInt(args[0]);//6000
         System.out.println("listening...");
