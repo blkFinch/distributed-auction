579e88d10abe67ebf8fdb80d3164b023afc31131
galen <blkfinch@gmail.com>
2020-11-25 12:21:21 -0700

    bank now saves new client to database

--- src/Bank/Bank.java
+++ src/Bank/Bank.java
@@ -1,6 +1,12 @@
 package Bank;
 
+import Database.DatabaseManager;
+
 import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Bank {
@@ -17,6 +23,14 @@ public class Bank {
         clients.add(newClient);
         System.out.println("added a new client at " + newClient.getHost() + " : " + newClient.getPort());
         System.out.println("balance of " + newClient.getBalance());
+
+        try(Connection conn = DriverManager.getConnection(DatabaseManager.DB_URL)){
+            newClient.save(conn);
+            System.out.println("client saved to database");
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+
     }
 
 
--- src/Bank/Client.java
+++ src/Bank/Client.java
@@ -1,13 +1,21 @@
 package Bank;
 
 import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-//DEPRECATED -just need account
+/** Model class for the Client table in the database
+ * -g. hutchison
+ */
 public class Client {
-    Account account;
     private int portNumber;
     private InetAddress host;
+    private int balance;
+    private String name;
+    private boolean isAuctionHouse;
 
+    //<editor-fold desc="GETTERS SETTERS">
     public int getPort(){
         return portNumber;
     }
@@ -17,12 +25,67 @@ public class Client {
     }
 
     public int getBalance(){
-        return account.getBalance();
+        return this.balance;
     }
 
-    public Client(InetAddress host,int port){
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAuctionHouse() {
+        return isAuctionHouse;
+    }
+
+    public void setAuctionHouse(boolean auctionHouse) {
+        isAuctionHouse = auctionHouse;
+    }
+
+    //</editor-fold>
+
+    public Client(InetAddress host, int port){
         this.portNumber = port;
         this.host = host;
-        this.account = new Account();
+        this.balance = 0;
+    }
+
+    public Client save(Connection conn){
+        String sql = "INSERT INTO clients " +
+                "   (host, port, balance, isAuctionHouse, name) \n" +
+                "   VALUES (?,?,?,?,?);";
+
+        try(PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1, String.valueOf(this.host));
+            pstmt.setInt(2, this.portNumber);
+            pstmt.setInt(3, this.balance);
+            //DEBUG: dummy values for now
+            pstmt.setBoolean(4, true);
+            pstmt.setString(5, "my auction house");
+
+            pstmt.execute();
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+        return this;
+    }
+
+    public static void create(){
+
+    }
+
+    public static void read(){
+
+    }
+
+    public static void update(){
+
+    }
+
+    public static void drestroy(){
+
     }
 }
