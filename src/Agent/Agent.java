694f84f290ebfddbf966a1f7484632d8cb7ea788
Ashley K <buginla@gmail.com>
2020-11-27 14:56:11 -0700

    Added AgentProxy class. Agent can now connect to the BankServer

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -1,4 +1,36 @@
 package Agent;
 
-public class Agent {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Agent{
+    private static AgentProxy bankProxy;
+
+    public static void main(String[] args){
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        String host;
+        int port;
+        boolean connected = false;
+
+        while(!connected){
+            System.out.println("What is the host IP?");
+            try{
+                host = in.readLine();
+            } catch(IOException e){
+                host = "localhost";
+            }
+            System.out.println("What is the port?");
+            try{
+                input = in.readLine();
+                port = Integer.parseInt(input);
+                bankProxy = new AgentProxy("bank", host, port);
+                connected = true;
+            } catch(Exception e){
+                System.out.println("Connection failed. Try again.");
+            }
+        }
+        System.out.println("You are now connected.");
+    }
 }
