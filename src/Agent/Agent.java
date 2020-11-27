b9e2a3e88aa53b3dfe19d34a052e3120f5c3de0b
Ashley K <buginla@gmail.com>
2020-11-27 15:12:29 -0700

    Agent can now choose to connect to a bank or an auction house

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -6,15 +6,23 @@ import java.io.InputStreamReader;
 
 public class Agent{
     private static AgentProxy bankProxy;
+    private static AgentProxy auctionProxy;
 
     public static void main(String[] args){
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
         String input;
+        String type;
         String host;
         int port;
         boolean connected = false;
 
         while(!connected){
+            System.out.println("What type of server are you connecting to?");
+            try{
+                type = in.readLine();
+            } catch(IOException e){
+                type = "none";
+            }
             System.out.println("What is the host IP?");
             try{
                 host = in.readLine();
@@ -25,7 +33,7 @@ public class Agent{
             try{
                 input = in.readLine();
                 port = Integer.parseInt(input);
-                bankProxy = new AgentProxy("bank", host, port);
+                bankProxy = new AgentProxy(type, host, port);
                 connected = true;
             } catch(Exception e){
                 System.out.println("Connection failed. Try again.");
--- src/Agent/AgentProxy.java
+++ src/Agent/AgentProxy.java
@@ -8,9 +8,15 @@ public class AgentProxy{
 
     public AgentProxy(String type, String host, int port) throws IOException{
         if(type.equals("bank")){ bankClient(host, port); }
+        else if(type.equals("auction")){ auctionClient(host, port); }
+        else{ throw new IOException(); }
     }
 
     private void bankClient(String host, int port) throws IOException{
+        socket = new Socket(host, port);
+    }
+
+    private void auctionClient(String host, int port) throws IOException{
         socket = new Socket(host, port);
     }
 }
