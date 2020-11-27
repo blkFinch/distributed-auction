4eb36ad0f3c0b6ea0228ab7f57629a55727cc64d
Ashley K <buginla@gmail.com>
2020-11-27 15:21:14 -0700

    Agent now has Lists to hold messages sent to and from server. Currently Lists are not used. Fixed my name in the README.

--- src/Agent/AgentProxy.java
+++ src/Agent/AgentProxy.java
@@ -2,11 +2,17 @@ package Agent;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AgentProxy{
     private Socket socket;
+    private List<String> inMessages;
+    private List<String> outMessages;
 
     public AgentProxy(String type, String host, int port) throws IOException{
+        inMessages = new ArrayList<>();
+        outMessages = new ArrayList<>();
         if(type.equals("bank")){ bankClient(host, port); }
         else if(type.equals("auction")){ auctionClient(host, port); }
         else{ throw new IOException(); }
@@ -19,4 +25,6 @@ public class AgentProxy{
     private void auctionClient(String host, int port) throws IOException{
         socket = new Socket(host, port);
     }
+
+    public void sendMessage(String message){ outMessages.add(message); }
 }
